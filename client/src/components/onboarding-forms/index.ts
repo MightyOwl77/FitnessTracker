f152533d042033828dd6564b2f0308fb4fd507d7
hp4f9z4mtk <39688734-hp4f9z4mtk@users.noreply.replit.com>
2025-03-09 19:26:31 +0000

    Restored to '3403045fae7d651250171cd9db55d74f6040b78c'
    Replit-Restored-To: 3403045fae7d651250171cd9db55d74f6040b78c

--- client/src/components/onboarding-forms/index.ts
+++
@@ -1,6 +0,0 @@
-export { WelcomeStep } from './WelcomeStep';
-export { ProfileForm } from './ProfileForm';
-export { GoalsForm } from './GoalsForm';
-export { DeficitPlanForm } from './DeficitPlanForm';
-export { PreferencesForm } from './PreferencesForm';
-export { CompleteStep } from './CompleteStep';
